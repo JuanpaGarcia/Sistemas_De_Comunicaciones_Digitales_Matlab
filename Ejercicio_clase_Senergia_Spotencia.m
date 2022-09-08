2b71504c52077d361b1bb9627eb77f9b32134176
JuanpaGarcia <garciaperezjuanpablo7@gmail.com>
2022-09-08 17:52:34 -0500

    Finale ejerccicio class

--- Ejercicio_clase_Senergia_Spotencia.m
+++ Ejercicio_clase_Senergia_Spotencia.m
@@ -14,3 +14,31 @@ Power = (sigP*sigP')/numel(sigP);
 Rxx = xcorr(sigP,sigP);
 plot(Rxx)
 histogram(sigP,'Normalization','pdf');
+
+%%
+%ejemplo como pa tarea jijij
+
+%Grafique el espectro de un pulso cuadrado
+Fs = 100;
+Ts = 1/Fs;
+t = 0:Ts:1;
+x = zeros(1,length(t));
+x( (t>=0.4) & (t<0.6) ) = 1;
+title('Pulso cuadrado, duración = 0.2s');
+% Analice el Ancho de Banda (según criterios)
+
+wvtool(x)%windows view tool
+% caida 3 db en freq 0.03125*Fs/2
+
+%%
+%SEÑALES DE POTENCIA 
+% Power Signal
+sigP= randn(1,1e6);
+Power = (sigP*sigP')/numel(sigP);
+Rxx = xcorr(sigP,sigP);
+plot(Rxx)
+histogram(sigP,'Normalization','pdf'); 
+%Eestimar la densidad de potencia con
+pwelch(x,500,300,500,Fs,'power')
+
+
