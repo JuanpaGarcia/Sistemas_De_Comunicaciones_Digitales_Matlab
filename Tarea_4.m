1cffd93f6d3b19405642221c19bf1f937bb4229c
JuanpaGarcia <garciaperezjuanpablo7@gmail.com>
2022-09-17 17:51:56 -0500

    Graficas

--- Tarea_4.m
+++ Tarea_4.m
@@ -65,11 +65,11 @@ for i = 1:mp
     end
 end
 stem(PRZ)
-wvtool(wvtool) 
+wvtool(PRZ) 
 
 %%
 %Manchester
-Manchester= zeros(1,mp); 
+Manchester = zeros(1,mp); 
 for i = 1:mp
     if i <= mp/2
         Manchester(i) = 1;  
@@ -79,31 +79,44 @@ for i = 1:mp
 end
 stem(Manchester)
 wvtool(Manchester) 
+
+
+
+
 %%
 %Generación de las señales
 
 V_16bit_polar = zeros(1,numel(V_16bit)*mp);
 V_16bit_polar(1:mp:end) = V_16bit*2-1; 
 
+
 V_16bit_Unipolar = zeros(1,numel(V_16bit)*mp);
-V_16bit_polar(1:mp:end) = V_16bit;
+V_16bit_Unipolar(1:mp:end) = V_16bit;
 %%
 %Señal con Unipolar NRZ
 Unipolar_NRZ_Sig = conv(UPNRZ ,V_16bit_Unipolar);
-plot(Unipolar_RZ_Sig)
+plot(Unipolar_NRZ_Sig)
 
 %%
 %Selak Polar NRZ
 Polar_NRZ_Sig = conv(UPNRZ ,V_16bit_polar);
-plot(Unipolar_RZ_Sig)
+plot(Polar_NRZ_Sig)
 
+%%
 %Señal Polar RZ
+Polar_RZ_sig = conv(PRZ, V_16bit_polar);
+plot(Polar_RZ_sig)
 
-
+%%
 %Señal Bipolar NRZ
+Bipolar_NRZ = conv(UPNRZ, V_16bit_polar);
+plot(Bipolar_NRZ);
 
-
+%%
 %Señal Manchester
+Manchester = conv(Manchester, V_16bit_polar);
+plot(Manchester);
+
 
 
 
